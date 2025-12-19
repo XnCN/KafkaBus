0d2d02bc4fd95410908d3eab766f2a2d51ae3569
Eray Han <erayhan41@yandex.com>
2025-12-19 17:06:52 +0300

    arch:default value deserializator added

---
+++ src/KafkaBus.Core/Consumer/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaBus.Core.Consumer;
+
+internal class Class1
+{
+}
---
+++ src/KafkaBus.Core/Serialization/DefaultValueDeserializer.cs
@@ -0,0 +1,15 @@
+﻿using Confluent.Kafka;
+using KafkaBus.Abstractions.Serialization;
+using System.Text.Json;
+
+namespace KafkaBus.Core.Serialization;
+
+public class DefaultValueDeserializer<T> : IDeserializer<T>, IDefaultValueDeserializer
+{
+    public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+        if (isNull || data.IsEmpty)
+            return default!;
+        return JsonSerializer.Deserialize<T>(data)!;
+    }
+}
